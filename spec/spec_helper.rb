f878a8f99c0eacb440a18b98470f1b35546b3d47
rtib <rtib@users.noreply.github.com>
2017-01-06 19:38:07 +0100

    coverage reporting moved to own spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,4 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'coveralls'
 
-RSpec.configure do |config|
-  config.after(:suite) do
-    Coveralls.wear!
-    RSpec::Puppet::Coverage.report!
-  end
-end
+Coveralls.wear!
