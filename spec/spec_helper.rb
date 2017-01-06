1252ea209dc8a482f9edb457e6c4ad684ed6e7ca
rtib <rtib@users.noreply.github.com>
2017-01-06 13:44:00 +0100

    coveralls reporting changed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,9 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'coveralls'
 
-Coveralls.wear!
-
 RSpec.configure do |config|
   config.after(:suite) do
     RSpec::Puppet::Coverage.report!
+    Coveralls.wear!
   end
 end
