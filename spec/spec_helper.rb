76f95ee16ddc5671a2e6978f00adafa45598c5af
rtib <rtib@users.noreply.github.com>
2017-01-10 22:02:36 +0100

    throw away coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,2 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
-require 'coveralls'
-include RspecPuppetFacts
-
-Coveralls.wear! do
-  add_filter '/spec/fixtures/modules/kibana5/'
-end
